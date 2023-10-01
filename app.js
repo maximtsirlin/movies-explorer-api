2cdba8f8c7e867d907ab0691d7a3172ad6f49e89
Maxim Tsirlin <maximtsirlin@gmail.com>
2023-10-01 15:21:43 +0300

    add cors

--- app.js
+++ app.js
@@ -22,7 +22,14 @@ mongoose.connect(addressMongoDB);
 
 app.use(helmet());
 app.use(bodyParser.json());
-app.use(cors);
+// app.use(cors);
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
