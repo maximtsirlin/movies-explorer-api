ca0f65a620b364718d3461e900d05e9a3a2f82b1
Maxim Tsirlin <maximtsirlin@gmail.com>
2023-10-08 18:37:57 +0300

    change localhost

--- utils/constants.js
+++ utils/constants.js
@@ -1,12 +1,12 @@
 const {
   NODE_ENV,
   JWT_SECRET,
-  DATA_BASE = 'mongodb://mongodb:27017/bitfilmsdb',
+  DATA_BASE = 'mongodb://localhost:27017/bitfilmsdb',
 } = process.env;
 
 const secretKey = NODE_ENV === 'production' ? JWT_SECRET : 'secret-key';
 
-const addressMongoDB = NODE_ENV === 'production' ? DATA_BASE : 'mongodb://mongodb:27017/bitfilmsdb';
+const addressMongoDB = NODE_ENV === 'production' ? DATA_BASE : 'mongodb://localhost:27017/bitfilmsdb';
 
 module.exports = {
   addressMongoDB,
